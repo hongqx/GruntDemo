f386ded4cd4804645cdf3377748f8f9eee8fc803
hongqx <hongqiongxing@163.com>
2015-08-05 18:55:18 +0800

    add the use of angularjs

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,11 @@
-define([], function() {
-  return {
-    hello: function() {
-      alert("hello, requirejs");
-    }
-  };
+define(['angular'], function(angular) {
+    angular.module('myApp', [])
+      .controller('MyController', ['$scope', function ($scope) {
+        $scope.name = 'Change the name';
+        $scope.age = 26;
+      }]);
+
+    angular.element(document).ready(function() {
+      angular.bootstrap(document, ['myApp']);
+    });
 });
--- public/js/config.js
+++ public/js/config.js
@@ -3,12 +3,10 @@ requirejs.config({
 	paths:{
 		app:"app",
 		jquery:"lib/jquery/jquery",
-		angular:"lib/angularjs/angular"
+		angular:"lib/dist/angular.min"
 	},
 	shim:{
-
+        angular:{exports:"angular"}//在全局中添加一个angular的变量，在shim中显示的暴露出来才能使用它
 	}
 });
-requirejs(["app"],function(app){
-	app.hello();
-});
+requirejs(['app']);
